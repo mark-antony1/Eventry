04d253076131f1fe9f2d4ce16bf3b3bd036d8ad2
15chrjef <15chrjef@gmail.com>
2020-03-02 18:43:38 -0800

    add location, update location, update events to be list

--- src/constants/locations.js
+++ src/constants/locations.js
@@ -1,13 +1,13 @@
 export const venues = [
 	{
-    "id": '1',
+		"id": 1,
 		"photos": [
 			"https://image.cnbcfm.com/api/v1/image/106033668-1563864541401sandbox.jpeg?v=1563864607&w=740&h=416",
 			"https://image.cnbcfm.com/api/v1/image/106033677-1563864964164concept1.png?v=1563865076&w=740&h=416"
 		],
 		"name": "Sandbox VR",
 		"description": "Go on thrilling adventures, transform into heroic characters. With your friends. See and interact inside VR just like it's real life. Great for Teams. See your friends in VR. Full-Body VR. The Star Trek Holodeck. Real-time Motion Capture. The Matrix. Hollywood Equipment.",
-		"events": "Immersive VR Team Experience",
+		"events": ["Immersive VR Team Experience"],
 		"hours": {
 			"Sunday": { "start": 10, "end": 22 },
 			"Monday": { "start": 10, "end": 22 },
@@ -23,56 +23,90 @@ export const venues = [
 		"address": "767 Market St, San Francisco, CA 94103",
 		"parkingAvailable": false,
 		"linkToSite": "https://sandboxvr.com/sanfrancisco/",
-		"tags": ["athletic", "game"],
+		"tags": ["indoor", "athletic", "games"],
 		"rating": 4.7,
 		"takesReservations": true,
 		"averageTimeSpent": 60,
-		"type": "game",
-		"place": 'indoor',
+		"indoor": true,
 		"recommendedGroupsize": [4, 6],
-		"price": "$",
+		"price": "$$$",
 		"location": {
-			"latitude": 37.786214,
-			"longitude": -122.404641
+			"latitude": 37.786426,
+			"longitude": -122.404609
 		},
 		"symbol": "sandbox-vr-sf"
 	},
-  {
-    "id": '2',
+	{
+		"id": 2,
 		"photos": [
-			"https://image.cnbcfm.com/api/v1/image/106033668-1563864541401sandbox.jpeg?v=1563864607&w=740&h=416",
-			"https://image.cnbcfm.com/api/v1/image/106033677-1563864964164concept1.png?v=1563865076&w=740&h=416"
+			"https://static.urbandaddy.com/uploads/assets/image/articles/standard/47a78b7f97119391669f6fbb268b46317352f69c.jpg",
+			"https://infatuation.imgix.net/media/images/reviews/local-edition/banners/1525311442.75.jpg?auto=format&h=840&w=1336"
 		],
-		"name": "Sandbox VR2",
-		"description": "Go on thrilling adventures, transform into heroic characters. With your friends. See and interact inside VR just like it's real life. Great for Teams. See your friends in VR. Full-Body VR. The Star Trek Holodeck. Real-time Motion Capture. The Matrix. Hollywood Equipment.",
-		"events": "Immersive VR Team Experience",
+		"name": "Local Edition",
+		"description": "An extensive cocktail menu is offered at this underground lounge with a vintage newspaper theme.",
+		"events": ["drink", "music"],
 		"hours": {
-			"Sunday": { "start": 10, "end": 22 },
-			"Monday": { "start": 10, "end": 22 },
-			"Tuesday": { "start": 10, "end": 22 },
-			"Wednesday": { "start": 10, "end": 22 },
-			"Thursday": { "start": 10, "end": 22 },
-			"Friday": { "start": 10, "end": 22 },
-			"Saturday": { "start": 10, "end": 22 }
+			"Sunday": { "start": 0, "end": 0 },
+			"Monday": { "start": 17, "end": 26 },
+			"Tuesday": { "start": 17, "end": 26 },
+			"Wednesday": { "start": 17, "end": 26 },
+			"Thursday": { "start": 17, "end": 26 },
+			"Friday": { "start": 16.5, "end": 26 },
+			"Saturday": { "start": 19, "end": 26 }
 		},
-		"phoneNumber": "(415) 625-0995",
+		"phoneNumber": "(415) 795-1375",
 		"email": "",
-		"vibe": ["Fun", "Exciting"],
-		"address": "767 Market St, San Francisco, CA 94103",
+		"vibe": ["Chill"],
+		"address": "691 Market St, San Francisco, CA 94105",
 		"parkingAvailable": false,
-		"linkToSite": "https://sandboxvr.com/sanfrancisco/",
-		"tags": ["outdoor", "athletic", "game"],
-		"rating": 4.7,
+		"linkToSite": "https://www.localeditionsf.com/",
+		"tags": ["indoor"],
+		"rating": 4.5,
 		"takesReservations": true,
 		"averageTimeSpent": 60,
-		"type": "game",
-		"place": 'outdoor',
-		"recommendedGroupsize": [4, 6],
-		"price": "$",
+		"indoor": true,
+		"recommendedGroupsize": [4, 14],
+		"price": "$$",
 		"location": {
-			"latitude": 37.786214,
-			"longitude": -122.304641
+			"latitude": 37.787777,
+			"longitude": -122.403161
 		},
 		"symbol": "sandbox-vr-sf"
-	}
-];
+	},
+	{
+		"id": 3,
+		"photos": [
+			"https://www.blueandgoldfleet.com/wp-content/uploads/2015/04/home_Carousel_BayCruise.jpg"
+		],
+		"name": "Blue and Gold Fleet",
+		"description": "Ferry and Boat tour operator in San Francisco, California",
+		"events": ["ferry to island", "ferry across the bay", "drink", "sightseeing"],
+		"hours": {
+			"Sunday": { "start": 9, "end": 18.45 },
+			"Monday": { "start": 9, "end": 18.45 },
+			"Tuesday": { "start": 9, "end": 18.45 },
+			"Wednesday": { "start": 9, "end": 18.45 },
+			"Thursday": { "start": 9, "end": 18.45 },
+			"Friday": { "start": 9, "end": 18.45 },
+			"Saturday": { "start": 9, "end": 18.45 }
+		},
+		"phoneNumber": "(415) 705-8200",
+		"email": "",
+		"vibe": ["chill", "fun", "adventurous"],
+		"address": "Pier 41, San Francisco, CA 94133",
+		"parkingAvailable": false,
+		"linkToSite": "http://www.blueandgoldfleet.com/",
+		"tags": ["outdoor", "water", "boat"],
+		"rating": 4.5,
+		"takesReservations": true,
+		"averageTimeSpent": 35,
+		"indoor": true,
+		"recommendedGroupsize": [0, 100],
+		"price": "$",
+		"location": {
+			"latitude": 37.809205,
+			"longitude": -122.411324
+		},
+		"symbol": "sandbox-vr-sf"
+	},
+]
